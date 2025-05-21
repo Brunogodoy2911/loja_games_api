614b987339995aa59313bd95b82ff250ddd0ecd7
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-05-21 13:10:59 -0300

    :tada: Initial Commit

---
+++ src/main/java/com/generations/loja_games_api/LojaGamesApiApplication.java
@@ -0,0 +1,13 @@
+package com.generations.loja_games_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaGamesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaGamesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generations/loja_games_api/LojaGamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generations.loja_games_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaGamesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
